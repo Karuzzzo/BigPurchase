d44bbe55d2df848891618269aa74b61f174f426b
Karuzzzo <clastone@mail.ru>
2021-01-08 21:57:47 +0300

    Merging versions

--- contracts/BigPurchase.sol
+++ contracts/BigPurchase.sol
@@ -56,23 +56,7 @@ contract BigPurchase{
     }
 
     function addProduct(string memory _name, uint _price, uint _amount, uint _treshold) public OnlyOwner {
-<<<<<<< HEAD
-        bool productExist = false;
-        
-        I have no idea why, but code down here doesnt work 
-        //if we already have same product with same price, we add new product to existing
-        if(ProductsCount >= 1 )
-            for(uint a = 0; ((a <= ProductsCount) && (!productExist)); a.add(1)){       
-                if(Products[a].Price == _price)
-                    if(Products[a].Treshold == _treshold)
-                        if(keccak256(abi.encodePacked(Products[a].Name)) == keccak256(abi.encodePacked(_name))){
-                            Products[a].Amount.add(_amount);        //we cant compare strings directly, so compare their hashes
-                            productExist = true;
-                        }
-            }
 
-        if(!productExist){
-=======
         require((bytes(_name).length > 0) && (_price > 0) && _amount > 0);
         bytes32 hashedProduct = getHashedProduct(_name, _price, _treshold);
 
